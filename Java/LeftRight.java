3542956c4d0a14e5eee0a3cc67114448ea912c9e
snaolekar <satyam,naolekar@gmail.com>
2017-11-04 17:18:05 +0530

    ceckin

---
+++ Java/LeftRight.java
@@ -0,0 +1,76 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class LeftRight {
+    static void swap(int arr[], int i , int j){
+        int temp= arr[i];
+        arr[i]= arr[j];
+        arr[j]= temp;
+        return ;
+    }
+    
+    static void sort(int arr[], int i, int j , boolean order){
+        //order true acseding false decending
+        int size= j-i+1;
+        int tmpArr[]= new int[size];
+        for(int i1=i,j1=0;i1<=j;i1++,j1++)
+            tmpArr[j1]=arr[i1];
+            
+        if(order)
+            Arrays.sort(tmpArr);
+        else{
+            for(int k=0;k<size;k++)
+                tmpArr[k]= tmpArr[k]*-1 ;
+            Arrays.sort(tmpArr);
+            for(int k=0;k<size;k++)
+                tmpArr[k]= tmpArr[k]*-1 ;
+        }
+        for(int i1=i,j1=0;i1<=j;i1++,j1++)
+            arr[i1]=tmpArr[j1];
+    }
+	public static void main (String[] args) {
+		Scanner sc= new Scanner(System.in); // Make a note of this
+	    int T= sc.nextInt();
+	    for(int t=0;t<T;t++){
+	        int size= sc.nextInt();
+	        int arr[]= new int[size];
+	        for(int i=0;i<size;i++){
+	            arr[i]=sc.nextInt();
+	        }
+	        int left=0;
+	        int right=size-1;
+	        while(left<right){
+	            while(left < size && arr[left] % 2 ==1)
+	                left++;
+	            while(right >=0 && arr[right] % 2 == 0)
+	                right--;
+	            if(left < right){
+	                swap(arr, left, right);
+	                left ++;
+	                right -- ;
+	            }
+          }
+          if(arr[left] %2 ==1 )
+            left++;
+          System.out.println(Arrays.toString(arr));
+	        
+	       // left will be first even number if there is any
+	       if(left == 0) // only even number 
+	            Arrays.sort(arr);
+	       else if(left ==size){
+          for(int k=0;k<size;k++)
+            arr[k]= arr[k]*-1 ;
+          Arrays.sort(arr);
+          for(int k=0;k<size;k++)
+            arr[k]= arr[k]*-1 ;
+         }
+	       else{
+	           sort(arr,0,left-1,false);
+	           sort(arr,left,size-1,true);
+         }
+         System.out.println(Arrays.toString(arr));
+        }
+	    sc.close();
+	}
+}
---
+++ Java/Spiral.java
@@ -0,0 +1,71 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Sprial {
+public static void main(String[] args) {
+     Scanner sc= new Scanner(System.in); // Make a note of this
+     int n = sc.nextInt();
+     int i=0,j=0;
+     int mat[][]= new int[n][n] ;
+     for(i=0;i<n;i++){
+       for (j=0;j<n;j++){
+         mat[i][j]= sc.nextInt();
+       }
+     }
+     if(n==1){
+        System.out.print(mat[i][j]);
+        sc.close();
+        return;
+     }
+     i=0;
+     j=0;
+     int dir=1 ;// 0 down 1 up
+         System.out.print(mat[i][j]+" ");
+         j++;
+         System.out.print(mat[i][j]+" ");
+         dir=0 ;
+        while(!(i==n-1 && j==n-1)){
+         while(dir==0 && !(i==n-1 && j==n-1)){
+             //go ginonally down
+             i++;
+             j--;
+             System.out.print(mat[i][j]+" ");
+            if(i==n-1){
+                j++;
+                System.out.print(mat[i][j]+" ");
+                dir=1;
+            }
+            else if(j==0){
+                if(i==n-1)
+                    j++;
+                else
+                    i++;
+                System.out.print(mat[i][j]+" ");
+                dir=1;
+            }
+         }
+         while(dir==1 &&  !(i==n-1 && j==n-1)){
+             //go ginonally up
+             i--;
+             j++;
+             System.out.print(mat[i][j]+" ");
+            if(i==0){
+                if(j==n-1)
+                    i++;
+                else
+                    j++;
+                System.out.print(mat[i][j]+" ");
+                dir=0;
+            }
+            else if(j==n-1){
+                i++;
+                System.out.print(mat[i][j]+" ");
+                dir=0;
+            }
+         }
+     }
+     sc.close();
+     return;
+}
+}
